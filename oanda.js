8bced8083e2bceb0339276f6b49f3cc4a914bbcb
Marc Robert Poulin <mrpoulin@oanda.com>
2013-08-06 13:40:51 -0400

    Tests using latest version of jQuery.
    Updated test.html; added test for getting instruments and changing orders.

--- oanda.js
+++ oanda.js
@@ -55,7 +55,7 @@ OANDA.transaction.list = function(accountId, callback) {
 }
 
 OANDA.transaction.listSpecific = function(accountId, transId, callback) {
-    OANDA.api("/v1/accounts/" + accountId + "/transactions/" + transId, 'GET', callback);
+    OANDA.api("/v1/accounts/" + accountId + "/transactions/" + transId, 'GET', {}, callback);
 }
 
 OANDA.trade = OANDA.trade || {};
@@ -65,15 +65,7 @@ OANDA.trade.list = function(accountId, callback) {
 }
 
 OANDA.trade.listSpecific = function(accountId, tradeIds, callback) {
-    var needComma = false;
-    var tradesStr = "";
-    for(var cur in tradeIds) {
-        if(needComma) {
-            tradesStr += ",";
-        }
-        tradesStr += tradeIds[cur];
-        needComma = true;
-    }
+    var tradesStr = tradeIds.join(',');
     OANDA.api("/v1/accounts/" + accountId + "/trades", 'GET', {ids : tradesStr}, callback);
 }
 
@@ -85,7 +77,7 @@ OANDA.trade.close = function(accountId, tradeId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/trades/" + tradeId, 'DELETE', {}, callback);
 }
 
-OANDA.trade.change = function(accountId, tradeId, stopLoss, takeProfit, trailingStop, callback) {
+OANDA.trade.change = function(accountId, tradeId, trailingStop, takeProfit, stopLoss, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/trades/" + tradeId, 'PUT', 
             { stopLoss : stopLoss, takeProfit : takeProfit, trailingStop : trailingStop },
             callback);
@@ -98,15 +90,7 @@ OANDA.order.list = function(accountId, callback) {
 }
 
 OANDA.order.listSpecific = function(accountId, orderIds, callback) {
-    var needComma = false;
-    var ordersStr = "";
-    for(var cur in orderIds) {
-        if(needComma) {
-            ordersStr += ",";
-        }
-        ordersStr += orderIds[cur];
-        needComma = true;
-    }
+    var ordersStr = orderIds.join(',');
     OANDA.api("/v1/accounts/" + accountId + "/orders", 'GET', {ids: ordersStr}, callback);
 }
 
@@ -119,8 +103,10 @@ OANDA.order.close = function(accountId, orderId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/orders/" + orderId, 'DELETE', {}, callback);
 }
 
-OANDA.order.change = function(accountId, orderId, units, price, callback) {
-    OANDA.api("/v1/accounts/" + accountId + "/orders/" + orderId, 'PUT', {units:units, price:price}, callback);
+OANDA.order.change = function(accountId, orderId, units, price, trailingStop, takeProfit, stopLoss, callback) {
+    OANDA.api("/v1/accounts/" + accountId + "/orders/" + orderId, 'PUT', 
+    {units:units, price:price, trailingStop : trailingStop, takeProfit : takeProfit, stopLoss : stopLoss}, 
+    callback);
 }
 
 OANDA.position = OANDA.position || {};
@@ -145,8 +131,9 @@ OANDA.account.register = function(currency, callback) {
 
 OANDA.rate = OANDA.rate || {};
 
-OANDA.rate.instruments = function(callback) {
-    OANDA.api("/v1/instruments", 'GET', {}, callback);
+OANDA.rate.instruments = function(fields, callback) {
+
+    OANDA.api("/v1/instruments", 'GET', {fields: fields.join(',')}, callback);
 }
 
 OANDA.rate.history = function(symbol, granularity, count, callback) {
@@ -154,16 +141,6 @@ OANDA.rate.history = function(symbol, granularity, count, callback) {
 }
 
 OANDA.rate.quote = function(symbols, callback) {
-    var needComma = false;
-    var symbolStr = "";
-    for(var cur in symbols) {
-        if(needComma) {
-            symbolStr += "%2C";
-        }
-        symbolStr += symbols[cur];
-        needComma = true;
-    }
-
+    var symbolStr = symbols.join(',');
     OANDA.api("/v1/quote", 'GET', {instruments: symbolStr}, callback);
 }
-
