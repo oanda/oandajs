049be258df9ef8db638bce10484e1e5764625146
Marc Robert Poulin <mrpoulin@oanda.com>
2013-08-06 15:21:04 -0400

    Added optional parameters to some requests.

--- oanda.js
+++ oanda.js
@@ -30,6 +30,8 @@ var sendAjaxRequest = function(endpoint, method, parameters, requiresAuth, onCom
     req.always(onComplete);
 }
 
+/* Send a raw api request on an endpoint.
+ */
 OANDA.api = function(endpoint, method, parameters, callback) {
 
     sendAjaxRequest(endpoint, method, parameters, true, function(jqXHR, textResponse) {
@@ -49,98 +51,179 @@ OANDA.transaction = OANDA.transaction || {};
 
 /* 
  * Lists all transactions for a specified account.
+ * Accepts no optional parameters.
  */
 OANDA.transaction.list = function(accountId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/transactions", 'GET', {}, callback);
 }
 
+/* List specific transactions by transaction id.
+ * Accepts no optional parameters
+ */
 OANDA.transaction.listSpecific = function(accountId, transId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/transactions/" + transId, 'GET', {}, callback);
 }
 
 OANDA.trade = OANDA.trade || {};
 
+/* List all trade in account.
+ * Accepts no optional parameters.
+ */
 OANDA.trade.list = function(accountId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/trades", 'GET', {}, callback);
 }
 
+/* List specific trades by id.
+ * Accepts no optional parameters
+ */
 OANDA.trade.listSpecific = function(accountId, tradeIds, callback) {
     var tradesStr = tradeIds.join(',');
     OANDA.api("/v1/accounts/" + accountId + "/trades", 'GET', {ids : tradesStr}, callback);
 }
 
+/* Open a new trade
+ * Accepts optional parameters:
+ * stopLoss     => number
+ * takeProfit   => number
+ * trailingStop => number
+ * upperBound   => number
+ * lowerBound   => number
+ * Accepts optional parameters
+ */
 OANDA.trade.open = function(accountId, instrument, units, side, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/trades", 'POST', {instrument:instrument, units:units, side:side}, callback);
 }
 
+/* Close an existing trade
+ * Accepts no optional parameters.
+ */
 OANDA.trade.close = function(accountId, tradeId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/trades/" + tradeId, 'DELETE', {}, callback);
 }
 
-OANDA.trade.change = function(accountId, tradeId, trailingStop, takeProfit, stopLoss, callback) {
-    OANDA.api("/v1/accounts/" + accountId + "/trades/" + tradeId, 'PUT', 
-            { stopLoss : stopLoss, takeProfit : takeProfit, trailingStop : trailingStop },
-            callback);
+/* Modfify and existing trade
+ * Accepts optional parameters:
+ *  stopLoss     => number
+ *  takeProfit   => number
+ *  trailingStop => number
+ */
+OANDA.trade.change = function(accountId, tradeId, optParameters, callback) {
+    OANDA.api("/v1/accounts/" + accountId + "/trades/" + tradeId, 'PUT', optParameters, callback);
 }
 
 OANDA.order = OANDA.order || {};
 
+
+/* List all orders in account.
+ * Accepts no optional parameters
+ */
 OANDA.order.list = function(accountId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/orders", 'GET', {}, callback);
 }
 
+/* List specific orders by id.
+ * Accepts no optional parameters
+ */
 OANDA.order.listSpecific = function(accountId, orderIds, callback) {
     var ordersStr = orderIds.join(',');
     OANDA.api("/v1/accounts/" + accountId + "/orders", 'GET', {ids: ordersStr}, callback);
 }
 
-OANDA.order.open = function(accountId, instrument, units, side, price, expiry, type, callback) {
-    OANDA.api("/v1/accounts/" + accountId + "/orders", 'POST', {instrument: instrument, units: units, side:side, price: price, expiry:expiry, type:type}, 
+/* Create a new order.
+ * Accepts optional parameters
+ * stopLoss     => number
+ * takeProfit   => number
+ * trailingStop => number
+ * upperBound   => number
+ * lowerBound   => number
+ */
+OANDA.order.open = function(accountId, instrument, units, side, price, expiry, type, optParameters, callback) {
+    OANDA.api("/v1/accounts/" + accountId + "/orders", 'POST', 
+              $.extend({instrument: instrument, units: units, side:side, price: price, expiry:expiry, type:type}, optParameters),
               callback);
 }
 
+/* Close an existing order.
+ * Accepts no optional parameters
+ */
 OANDA.order.close = function(accountId, orderId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/orders/" + orderId, 'DELETE', {}, callback);
 }
 
-OANDA.order.change = function(accountId, orderId, units, price, trailingStop, takeProfit, stopLoss, callback) {
-    OANDA.api("/v1/accounts/" + accountId + "/orders/" + orderId, 'PUT', 
-    {units:units, price:price, trailingStop : trailingStop, takeProfit : takeProfit, stopLoss : stopLoss}, 
-    callback);
+/* Modify an existing order
+ * Accepts optional parameters:
+ * units        => number
+ * price        => number
+ * expiry       => string
+ * lowerBound   => number
+ * upperBound   => number
+ * stopLoss     => number
+ * takeProfit   => number
+ * trailingStop => number
+ */
+OANDA.order.change = function(accountId, orderId, optParameters, callback) {
+    OANDA.api("/v1/accounts/" + accountId + "/orders/" + orderId, 'PUT', optParameters, callback);
 }
 
 OANDA.position = OANDA.position || {};
 
+/* List all positions
+ * Accepts no optional parameters
+ */
 OANDA.position.list = function(accountId, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/positions", 'GET', {}, callback);
 }
 
+/* List only position for specific instrument
+ * Accepts no optional parameters
+ */
 OANDA.position.listSpecific = function(accountId, instrument, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/positions/" + instrument, 'GET' ,{}, callback);
 }
 
+/* Close all trades in a positions
+ * Accepts no optional parameters
+ */
 OANDA.position.close = function(accountId, instrument, callback) {
     OANDA.api("/v1/accounts/" + accountId + "/positions/" + instrument, 'DELETE', {}, callback);
 }
 
 OANDA.account = OANDA.account || {};
 
+/* Register an account
+ * Accepts no optional parameters
+ */
 OANDA.account.register = function(currency, callback) {
     OANDA.api("/v1/accounts", 'POST', {currency:currency}, callback);
 }
 
 OANDA.rate = OANDA.rate || {};
 
+/* List all instruments available.
+ * Accepts optional parameters:
+ * fields => array of strings
+ */
 OANDA.rate.instruments = function(fields, callback) {
 
     OANDA.api("/v1/instruments", 'GET', {fields: fields.join(',')}, callback);
 }
 
-OANDA.rate.history = function(symbol, granularity, count, callback) {
-    OANDA.api("/v1/history", 'GET', {instrument : symbol, granularity : granularity, count : count}, callback);
+/* Return candlesticks for a specific instrument
+ * Accepts optional parameters:
+ * granularity  => string
+ * count        => number
+ * start        => string
+ * end          => string
+ * candleFormat => string
+ * includeFirst => boolean
+ */
+OANDA.rate.history = function(symbol, optParameters, callback) {
+    OANDA.api("/v1/history", 'GET', $.extend({instrument:symbol}, optParameters), callback);
 }
 
+/* Lists the current price for a list of instruments
+ * Accepts no optional parameters
+ */
 OANDA.rate.quote = function(symbols, callback) {
-    var symbolStr = symbols.join(',');
-    OANDA.api("/v1/quote", 'GET', {instruments: symbolStr}, callback);
+    OANDA.api("/v1/quote", 'GET', {instruments: symbols.join(',')}, callback);
 }
